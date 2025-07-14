c128619d52e9942f6f390b21b34aa2ea5325b6ff
Neil <neilhogg69@gmail.com>
2025-07-14 15:00:21 +0200

    Animations

--- wall-e/animations.ino
+++ wall-e/animations.ino
@@ -66,6 +66,82 @@ void playAnimation(int animationNo) {
 			queue.push({1000,  50,  10,   0,   0,   0,  40,  40});
 			break;
 
+		case 3:
+			// --- Happy Expression ---
+			//          time,head,necT,necB,eyeR,eyeL,armL,armR
+			queue.push({ 500,  50,  20,  30,  80,  80,  20,  20});
+			queue.push({ 800,  40,  30,  40, 100, 100,  10,  10});
+			queue.push({ 600,  60,  30,  40, 100, 100,  90,  90});
+			queue.push({ 800,  50,  20,  30,  80,  80,  20,  20});
+			queue.push({ 600,  45,  25,  35, 100, 100,  80,  80});
+			queue.push({ 600,  55,  25,  35, 100, 100,  10,  10});
+			queue.push({1200,  50,  20,  30,  80,  80,  40,  40});
+			break;
+
+		case 4:
+			// --- Sad Expression ---
+			//          time,head,necT,necB,eyeR,eyeL,armL,armR
+			queue.push({1500,  50,  60,  20,  10,  10,  60,  60});
+			queue.push({2000,  45,  70,  10,   0,   0,  70,  70});
+			queue.push({1800,  40,  80,   5,   5,   5,  80,  80});
+			queue.push({2500,  35,  85,   0,   0,   0,  85,  85});
+			queue.push({1500,  40,  80,   5,   5,   5,  80,  80});
+			queue.push({2000,  45,  70,  10,  10,  10,  70,  70});
+			queue.push({1500,  50,  60,  20,  20,  20,  60,  60});
+			break;
+
+		case 5:
+			// --- Surprise Expression ---
+			//          time,head,necT,necB,eyeR,eyeL,armL,armR
+			queue.push({ 200,  50,  10,  60, 100, 100,  40,  40});
+			queue.push({ 300,  40,   0,  80, 100, 100,  10,  10});
+			queue.push({ 400,  60,   5,  70, 100, 100,  90,  90});
+			queue.push({ 500,  50,  15,  50, 100, 100,  20,  80});
+			queue.push({ 600,  45,  10,  60, 100, 100,  80,  20});
+			queue.push({ 400,  55,   5,  65, 100, 100,  30,  70});
+			queue.push({1000,  50,  10,  50,  80,  80,  40,  40});
+			break;
+
+		case 6:
+			// --- Dance Movement ---
+			//          time,head,necT,necB,eyeR,eyeL,armL,armR
+			queue.push({ 800,  30,  30,  40,  60,  60,  90,  10});
+			queue.push({ 800,  70,  20,  50,  80,  80,  10,  90});
+			queue.push({ 600,  50,  40,  30,  40,  40,  80,  20});
+			queue.push({ 600,  50,  10,  60, 100, 100,  20,  80});
+			queue.push({ 800,  20,  30,  40,  60,  60,  90,  10});
+			queue.push({ 800,  80,  20,  50,  80,  80,  10,  90});
+			queue.push({ 600,  50,  35,  35,  70,  70,  50,  50});
+			queue.push({ 800,  40,  25,  45,  90,  90,  80,  20});
+			queue.push({ 800,  60,  25,  45,  90,  90,  20,  80});
+			queue.push({1000,  50,  30,  40,  80,  80,  40,  40});
+			break;
+
+		case 7:
+			// --- Sleep Animation ---
+			//          time,head,necT,necB,eyeR,eyeL,armL,armR
+			queue.push({2000,  50,  40,  30,  60,  60,  50,  50});
+			queue.push({2500,  48,  60,  20,  40,  40,  60,  60});
+			queue.push({3000,  45,  75,  10,  20,  20,  70,  70});
+			queue.push({3500,  42,  85,   5,  10,  10,  80,  80});
+			queue.push({4000,  40,  90,   0,   0,   0,  85,  85});
+			queue.push({1000,  40,  90,   0,   0,   0,  85,  85});
+			break;
+
+		case 8:
+			// --- Wake Up Animation ---
+			//          time,head,necT,necB,eyeR,eyeL,armL,armR
+			queue.push({2000,  40,  90,   0,   0,   0,  85,  85});
+			queue.push({1500,  42,  80,   5,  10,  10,  80,  80});
+			queue.push({1200,  45,  70,  10,  20,  20,  70,  70});
+			queue.push({1000,  48,  50,  20,  40,  40,  60,  60});
+			queue.push({ 800,  50,  30,  40,  60,  60,  50,  50});
+			queue.push({ 600,  52,  20,  50,  80,  80,  40,  40});
+			queue.push({ 500,  48,  15,  60, 100, 100,  30,  30});
+			queue.push({ 400,  52,  10,  65, 100, 100,  20,  20});
+			queue.push({1000,  50,  10,  60,  80,  80,  40,  40});
+			break;
+
 		default:
 			Serial.println(F("Invalid animation requested"));
 			break;
